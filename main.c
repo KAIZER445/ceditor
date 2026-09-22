825329743f9b5ab5b77ae028296bdbc32ad44162
KAIZER445 <prashanttamang861@gmail.com>
2026-09-22 21:55:29 +0545

    use perror

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ int main(int argc, char *argv[]){
     const char *path = (argc > 1) ? argv[1] : ".";
 
     DIR *dir = opendir(path);
-    printf("%s \n", path);
+    if(!dir) {
+        perror("opendir");
+        return 1;
+    }
     return 0;
 }
