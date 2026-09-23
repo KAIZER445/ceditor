4c37a5c6e9bfd4e07b562e6efba96b6c2c0b7c40
KAIZER445 <prashanttamang861@gmail.com>
2026-09-23 21:23:44 +0545

    feature: ls 22 line

--- main.c
+++ main.c
@@ -9,5 +9,14 @@ int main(int argc, char *argv[]){
         perror("opendir");
         return 1;
     }
+
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL){
+        if(entry->d_name[0] == '.') continue;
+        printf("%s \n", entry->d_name);
+    }
+
+    closedir(dir);
+
     return 0;
 }
